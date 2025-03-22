186e3d979c03b9953a3dea30329f3aab52cf6b85
golferjoe <versioncontrol.undamaged050@passinbox.com>
2025-03-22 02:12:59 +0100

    basic file parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,76 @@
-#include <cstdio>
+#include <format>
 #include <fstream>
-#include <iostream>
-#include <iterator>
+#include <print>
+#include <string>
+
+std::string trim(const std::string &str) {
+    auto start = str.find_first_not_of(" \t\n");
+    auto end = str.find_last_not_of(" \t\n");
+
+    if (start == std::string::npos) {
+        return "";
+    }
+
+    return str.substr(start, end - start + 1);
+}
+
+std::string parseLine(const std::string &line) {
+    // TODO: try to find a better way of implementing parsing instead of using 10000 if statements
+
+    const auto &trimmed = trim(line);
+
+    // extract tag
+    if (trimmed.starts_with("[h:")) {
+        // find tag closing bracket position
+        const auto closingBracket = trimmed.find(']');
+        if (closingBracket == std::string::npos) {
+            return "";
+        }
+
+        // extract tag type and text
+        const std::string tag = trimmed.substr(3, closingBracket - 3);
+        const std::string text = trim(trimmed.substr(closingBracket + 1));
+
+        // parse into html heading
+        if (tag == "big") {
+            return std::format("<h1>{}<h2>", text);
+        } else if (tag == "med") {
+            return std::format("<h2>{}<h2>", text);
+        } else if (tag == "sma") {
+            return std::format("<h3>{}<h3>", text);
+        }
+    }
+
+    return "";
+}
 
 int main(int argc, char *argv[]) {
-    // check if enough arguments were provided
-    if (argc < 2) {
-        printf("No file to transpile provided!\n");
+    // check if user provided enough arguments
+    if (argc < 3) {
+        std::print("Not enough arguments provided!\nSyntax: anl <source> <output>\n");
         return 1;
     }
 
-    // extract source file and check if it exists
-    const auto fileName = argv[1];
+    // extract args
+    const auto sourceName = argv[1];
+    const auto outputName = argv[2];
 
-    std::ifstream file(fileName);
-    if (!file.is_open()) {
-        printf("Failed to open source file! Does the file exists?\n");
+    // try to open input file
+    std::ifstream source(sourceName);
+    if (!source.is_open()) {
+        std::print("Failed to open source file! Does the file exists?\n");
         return 1;
     }
 
-    // read file contents
-    std::string content((std::istreambuf_iterator<char>(file)),
-                        (std::istreambuf_iterator<char>()));
+    // read file line by line
+    std::string line;
+    while (std::getline(source, line)) {
+        const auto &html = parseLine(line);
 
-    std::cout << content << std::endl;
+        if (!html.empty()) {
+            std::print("{}\n", html);
+        }
+    }
 
     return 0;
 }
